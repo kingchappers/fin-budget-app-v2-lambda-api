3c98a7f5041354f819d71af99d7237dfcd0d3883
kingchappers <sam.chapman@outlook.com>
2025-02-19 07:01:46 +0000

    Added role to lamda function

--- data.tf
+++ data.tf
@@ -10,3 +10,11 @@ data "aws_iam_policy_document" "assume_role" {
     actions = ["sts:AssumeRole"]
   }
 }
+
+data "aws_iam_policy_document" "create_income_dynamodb" {
+  statement {
+    effect    = "Allow"
+    actions   = ["dynamodb:PutItem"]
+    resources = ["arn:aws:dynamodb:eu-west-2:192350001975:table/testFinBudgetIncomeTable"]
+  }
+}
--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
+resource "aws_iam_role" "create_income_lambda_role" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
@@ -21,14 +21,30 @@ resource "aws_iam_role" "iam_for_lambda" {
 resource "aws_lambda_function" "create_income" {
   filename      = "./createIncome/createIncome.zip"
   function_name = "createIncome"
-  role          = aws_iam_role.lambda_exec.arn
+  role          = aws_iam_role.create_income_lambda_role.arn
 
   source_code_hash = filebase64sha256("./createIncome/createIncome.zip")
 
   runtime = "provided.al2023"
   handler = "bootstrap"
 
+  environment {
+    variables = {
+      INCOME_TABLE = "testFinBudgetIncomeTable"
+    }
+  }
+
   //Next items to define: 
-  // 3. Environment variable for INCOME_TABLE used in the createIncome.go function
-  // 4. Set the role for the lambda function (role)
+  // 4. Set the role for the lambda function to add values to dynamodb (role)
+}
+
+resource "aws_iam_policy" "create_income_dynamodb_policy" { 
+  name        = "create-income"
+  description = "A policy to allow the lambda function to add values to the income table"
+  policy      = data.aws_iam_policy_document.create_income_dynamodb.json
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.create_income_lambda_role.name
+  policy_arn = aws_iam_policy.create_income_dynamodb_policy.arn
 }
