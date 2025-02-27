bc2f1f245c6edef6698cc0407afa1dc61c55fc39
kingchappers <sam.chapman@outlook.com>
2025-02-27 06:52:21 +0000

    restructured jobs

---
+++ build/buildGoPackage.sh
@@ -0,0 +1,9 @@
+cd ../createIncome
+GOOS=linux GOARCH=arm64 go get "github.com/aws/aws-lambda-go/lambda"
+GOOS=linux GOARCH=arm64 go get "github.com/aws/aws-sdk-go-v2/aws"
+GOOS=linux GOARCH=arm64 go get "github.com/aws/aws-sdk-go-v2/config"
+GOOS=linux GOARCH=arm64 go get "github.com/aws/aws-sdk-go-v2/service/dynamodb"
+GOOS=linux GOARCH=arm64 go get "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+GOOS=linux GOARCH=arm64 go build -tags lambda.norpc -o bootstrap ./createIncome.go
+zip createIncome.zip bootstrap
+ls -la
