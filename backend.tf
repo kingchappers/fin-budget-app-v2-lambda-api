f2ef67d70752185f251c80d7a9565fa8e6efb8e4
kingchappers <sam.chapman@outlook.com>
2025-03-04 06:56:59 +0000

    updated bucket name

--- backend.tf
+++ backend.tf
@@ -1,8 +1,13 @@
 terraform {
   backend "s3" {
-    bucket         = "tfremotestate"
+    bucket         = "tfremotestate-fin-project"
     key            = "state"
     region         = "eu-west-2"
     dynamodb_table = "tfremotestate-lock"
   }
 }
+
+# I think the problem here is that the s3 bucket doesn't already exist. I'll create it manually and then try again.
+# I'll also need to create the dynamodb table.
+# I'll create the bucket first.
+# I'll create the dynamodb table next.
