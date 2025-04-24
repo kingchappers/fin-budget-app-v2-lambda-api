06dbbf3e1322ba266e18fdec5561b8d1cb60c00e
kingchappers <sam.chapman@outlook.com>
2025-04-24 07:49:10 +0100

    storing parameters in ssm

--- build/storeTerraformOutputs.sh
+++ build/storeTerraformOutputs.sh
@@ -2,3 +2,7 @@
 COGNITO_USER_POOL_ID=$(terraform output --json | jq -r '.cognito_user_pool_id.value')
 COGNITO_USER_POOL_CLIENT_ID=$(terraform output --json | jq -r '.cognito_user_pool_client_id.value')
 COGNITO_IDENTITY_POOL_ID=$(terraform output --json | jq -r '.cognito_identity_pool_id.value')
+
+aws ssm put-parameter --name "COGNITO_USER_POOL_ID" --value $COGNITO_USER_POOL_ID --type String
+aws ssm put-parameter --name "COGNITO_USER_POOL_CLIENT_ID" --value $COGNITO_USER_POOL_CLIENT_ID --type String
+aws ssm put-parameter --name "COGNITO_IDENTITY_POOL_ID" --value $COGNITO_IDENTITY_POOL_ID --type String
