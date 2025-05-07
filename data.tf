98d345dc3091f80b5aa3273686e56f73907a8445
kingchappers <sam.chapman@outlook.com>
2025-05-07 07:36:38 +0100

    updated policy document

--- data.tf
+++ data.tf
@@ -21,13 +21,8 @@ data "aws_iam_policy_document" "create_income_dynamodb" {
 
 data "aws_iam_policy_document" "api_gateway_assume_role" {
   statement {
-
     effect  = "Allow"
     actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["apigateway.amazonaws.com"]
-    }
+    resources = ["arn:aws:apigateway:eu-west-2:192350001975:*"]
   }
 }
