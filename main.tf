3f191885d207fccca8dbc68eb8625e80f2400320
kingchappers <sam.chapman@outlook.com>
2025-05-21 07:23:48 +0100

    adding the lambda function to the correct path

--- main.tf
+++ main.tf
@@ -262,7 +262,7 @@ resource "aws_api_gateway_method" "api_root" {
 
 resource "aws_api_gateway_integration" "api_root" {
   rest_api_id = aws_api_gateway_rest_api.fin_budget_api.id
-  resource_id = aws_api_gateway_rest_api.fin_budget_api.root_resource_id
+  resource_id = aws_api_gateway_resource.income_api_greedy_resource.id
   http_method = aws_api_gateway_method.api_root.http_method
 
   integration_http_method = "POST"
