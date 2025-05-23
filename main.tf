eebe51d6fa242c115be17d688009aabeec2ffe21
kingchappers <sam.chapman@outlook.com>
2025-05-23 07:03:20 +0100

    added dependancies

--- main.tf
+++ main.tf
@@ -262,7 +262,7 @@ resource "aws_api_gateway_method" "api_root" {
 
 resource "aws_api_gateway_integration" "api_root" {
   depends_on = [
-    aws_api_gateway_rest_api,
+    aws_api_gateway_rest_api.fin_budget_api,
     aws_api_gateway_resource.income_api_resource,
     aws_api_gateway_method.api_root,
   ]
