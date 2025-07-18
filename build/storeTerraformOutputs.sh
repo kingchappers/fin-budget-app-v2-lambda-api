b54684800006d06d7298bd4ca69be54430f59b42
kingchappers <sam.chapman@outlook.com>
2025-07-18 14:35:14 +0100

    updated ssm parameter names

--- build/storeTerraformOutputs.sh
+++ build/storeTerraformOutputs.sh
@@ -3,6 +3,6 @@ COGNITO_USER_POOL_ID=$(terraform output --json | jq -r '.cognito_user_pool_id.va
 COGNITO_USER_POOL_CLIENT_ID=$(terraform output --json | jq -r '.cognito_user_pool_client_id.value')
 COGNITO_IDENTITY_POOL_ID=$(terraform output --json | jq -r '.cognito_identity_pool_id.value')
 
-aws ssm put-parameter --name "/amplify/d3m9wu6rhd9z99/main/AMPLIFY_COGNITO_USER_POOL_ID" --value $COGNITO_USER_POOL_ID --type String --overwrite
-aws ssm put-parameter --name "/amplify/d3m9wu6rhd9z99/main/COGNITO_USER_POOL_CLIENT_ID" --value $COGNITO_USER_POOL_CLIENT_ID --type String --overwrite
-aws ssm put-parameter --name "/amplify/d3m9wu6rhd9z99/main/COGNITO_IDENTITY_POOL_ID" --value $COGNITO_IDENTITY_POOL_ID --type String --overwrite
+aws ssm put-parameter --name "/amplify/AMPLIFY_COGNITO_USER_POOL_ID" --value $COGNITO_USER_POOL_ID --type String --overwrite
+aws ssm put-parameter --name "/amplify/COGNITO_USER_POOL_CLIENT_ID" --value $COGNITO_USER_POOL_CLIENT_ID --type String --overwrite
+aws ssm put-parameter --name "/amplify/COGNITO_IDENTITY_POOL_ID" --value $COGNITO_IDENTITY_POOL_ID --type String --overwrite
