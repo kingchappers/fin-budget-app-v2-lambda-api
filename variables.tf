117f6b9265eacad80266df6ba63946c12bef9d3a
kingchappers <sam.chapman@outlook.com>
2025-07-26 14:42:47 +0100

    followed cors tutorial

--- variables.tf
+++ variables.tf
@@ -5,8 +5,8 @@ variable "region" {
   sensitive   = true
 }
 
-variable "allowed_origins" {
-  type        = string
-  description = "Comma-separated list of allowed origins for CORS"
-  default     = "https://main.d3m9wu6rhd9z99.amplifyapp.com,https://localhost:8080"
-}
+# variable "allowed_origins" {
+#   type        = string
+#   description = "Comma-separated list of allowed origins for CORS"
+#   default     = "https://main.d3m9wu6rhd9z99.amplifyapp.com,https://localhost:8080"
+# }
