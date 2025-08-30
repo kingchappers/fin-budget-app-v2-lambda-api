7883c1b49181b54b3758ab0d7dead308c2ce31bc
kingchappers <sam.chapman@outlook.com>
2025-08-30 13:35:13 +0100

    removed cors for test

--- main.tf
+++ main.tf
@@ -436,10 +436,10 @@ resource "aws_api_gateway_integration" "income_api_integration" {
 resource "aws_api_gateway_deployment" "api" {
   depends_on = [
     aws_api_gateway_integration.income_api_integration,
-    aws_api_gateway_integration.income_options_integration,
+    # aws_api_gateway_integration.income_options_integration,
     aws_api_gateway_integration.api_root,
     aws_api_gateway_method.income_post_method,
-    aws_api_gateway_method.income_options,
+    # aws_api_gateway_method.income_options,
     aws_api_gateway_method.api_root    
   ]
 
