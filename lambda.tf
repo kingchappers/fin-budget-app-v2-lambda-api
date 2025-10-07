13d6cb31facfcce6ca2aa1ae0ffd8447498f7522
kingchappers <sam.chapman@outlook.com>
2025-10-07 08:06:07 +0100

    build lambda function from Go code

--- lambda.tf
+++ lambda.tf
@@ -28,3 +28,34 @@ resource "aws_lambda_permission" "api" {
   # The following format is: arn:aws:execute-api:${region}:${account_id}:${api_id}/${stage_name}/${method}/${resource}
   source_arn = "${aws_api_gateway_rest_api.fin_budget_api.execution_arn}/*/income"
 }
+
+######################################################################
+# Get Income Lambda Function
+######################################################################
+
+resource "aws_lambda_function" "create_income" {
+  filename      = "./getIncome/getIncome.zip"
+  function_name = "getIncome"
+  role          = aws_iam_role.create_lambda_role.arn
+
+  source_code_hash = filebase64sha256("./getIncome/getIncome.zip")
+
+  runtime = "provided.al2023"
+  handler = "bootstrap"
+
+  environment {
+    variables = {
+      INCOME_TABLE = aws_dynamodb_table.income_table.name
+    }
+  }
+}
+
+resource "aws_lambda_permission" "api" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.create_income.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The following format is: arn:aws:execute-api:${region}:${account_id}:${api_id}/${stage_name}/${method}/${resource}
+  source_arn = "${aws_api_gateway_rest_api.fin_budget_api.execution_arn}/*/income"
+}
