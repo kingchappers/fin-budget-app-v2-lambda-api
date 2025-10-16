8122cce9b47b560fd3aa57f9d7f39f7a6a4a7bee
kingchappers <sam.chapman@outlook.com>
2025-10-16 08:14:45 +0100

    Added api root

--- apigateway.tf
+++ apigateway.tf
@@ -38,19 +38,19 @@ resource "aws_api_gateway_authorizer" "cognito_authorizer" {
   provider_arns                    = [aws_cognito_user_pool.fin_budget_user_pool.arn]
 }
 
-# resource "aws_api_gateway_method" "api_root" {
-#   depends_on = [
-#     aws_lambda_permission.create_income_api_permission,
-#     aws_api_gateway_authorizer.cognito_authorizer,
-#     aws_api_gateway_rest_api.fin_budget_api
-#   ]
+resource "aws_api_gateway_method" "api_root" {
+  depends_on = [
+    aws_lambda_permission.create_income_api_permission,
+    aws_api_gateway_authorizer.cognito_authorizer,
+    aws_api_gateway_rest_api.fin_budget_api
+  ]
 
-#   rest_api_id   = aws_api_gateway_rest_api.fin_budget_api.id
-#   resource_id   = aws_api_gateway_rest_api.fin_budget_api.root_resource_id
-#   http_method   = "ANY"
-#   authorization = "COGNITO_USER_POOLS"
-#   authorizer_id = aws_api_gateway_authorizer.cognito_authorizer.id
-# }
+  rest_api_id   = aws_api_gateway_rest_api.fin_budget_api.id
+  resource_id   = aws_api_gateway_rest_api.fin_budget_api.root_resource_id
+  http_method   = "ANY"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.cognito_authorizer.id
+}
 
 # resource "aws_api_gateway_integration" "api_root" {
 #   rest_api_id = aws_api_gateway_rest_api.fin_budget_api.id
