d47e2f1242f6501c24779abb90b9c76434d11013
kingchappers <sam.chapman@outlook.com>
2025-10-16 08:22:32 +0100

    Added api root

--- apigateway.tf
+++ apigateway.tf
@@ -52,15 +52,15 @@ resource "aws_api_gateway_method" "api_root" {
   authorizer_id = aws_api_gateway_authorizer.cognito_authorizer.id
 }
 
-# resource "aws_api_gateway_integration" "api_root" {
-#   rest_api_id = aws_api_gateway_rest_api.fin_budget_api.id
-#   resource_id = aws_api_gateway_rest_api.fin_budget_api.root_resource_id
-#   http_method = aws_api_gateway_method.api_root.http_method
+resource "aws_api_gateway_integration" "api_root" {
+  rest_api_id = aws_api_gateway_rest_api.fin_budget_api.id
+  resource_id = aws_api_gateway_rest_api.fin_budget_api.root_resource_id
+  http_method = aws_api_gateway_method.api_root.http_method
 
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri                     = aws_lambda_function.create_income.invoke_arn
-# }
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.create_income.invoke_arn
+}
 
 ######################################################################
 # Create API Gateway's Income Resources
