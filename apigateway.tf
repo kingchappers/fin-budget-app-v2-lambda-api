21f398795bad5f7cf2e355c81e9f61825136247c
kingchappers <sam.chapman@outlook.com>
2025-10-19 17:56:05 +0100

    removed unecessary root integration

--- apigateway.tf
+++ apigateway.tf
@@ -38,29 +38,29 @@ resource "aws_api_gateway_authorizer" "cognito_authorizer" {
   provider_arns                    = [aws_cognito_user_pool.fin_budget_user_pool.arn]
 }
 
-resource "aws_api_gateway_method" "api_root" {
-  depends_on = [
-    aws_lambda_permission.create_income_api_permission,
-    aws_api_gateway_authorizer.cognito_authorizer,
-    aws_api_gateway_rest_api.fin_budget_api
-  ]
+# resource "aws_api_gateway_method" "api_root" {
+#   depends_on = [
+#     aws_lambda_permission.create_income_api_permission,
+#     aws_api_gateway_authorizer.cognito_authorizer,
+#     aws_api_gateway_rest_api.fin_budget_api
+#   ]
 
-  rest_api_id   = aws_api_gateway_rest_api.fin_budget_api.id
-  resource_id   = aws_api_gateway_rest_api.fin_budget_api.root_resource_id
-  http_method   = "ANY"
-  authorization = "COGNITO_USER_POOLS"
-  authorizer_id = aws_api_gateway_authorizer.cognito_authorizer.id
-}
+#   rest_api_id   = aws_api_gateway_rest_api.fin_budget_api.id
+#   resource_id   = aws_api_gateway_rest_api.fin_budget_api.root_resource_id
+#   http_method   = "ANY"
+#   authorization = "COGNITO_USER_POOLS"
+#   authorizer_id = aws_api_gateway_authorizer.cognito_authorizer.id
+# }
 
-resource "aws_api_gateway_integration" "api_root" {
-  rest_api_id = aws_api_gateway_rest_api.fin_budget_api.id
-  resource_id = aws_api_gateway_rest_api.fin_budget_api.root_resource_id
-  http_method = aws_api_gateway_method.api_root.http_method
+# resource "aws_api_gateway_integration" "api_root" {
+#   rest_api_id = aws_api_gateway_rest_api.fin_budget_api.id
+#   resource_id = aws_api_gateway_rest_api.fin_budget_api.root_resource_id
+#   http_method = aws_api_gateway_method.api_root.http_method
 
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.create_income.invoke_arn
-}
+#   integration_http_method = "POST"
+#   type                    = "AWS_PROXY"
+#   uri                     = aws_lambda_function.create_income.invoke_arn
+# }
 
 ######################################################################
 # Create API Gateway's Income Resources
